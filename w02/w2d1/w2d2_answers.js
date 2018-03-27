ce3a5c5d7084b3e8cfca8d93ffa855bf7c01b678
vpdacoder <vaipatel@utexas.edu>
2018-03-26 19:17:53 -0500

    1st commit - selecting datatypes

---
+++ w02/w2d1/w2d2_answers.js
@@ -0,0 +1,26 @@
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// A light switch that can be either on or off.
+    // Booleans - true or false values
+// A user's email address.
+    // String
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    // An Array
+// A list of student names from our class.
+    // An Array
+// A list of student names from our class, each with a location.
+    // An Object nested in an Array
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    // An Object nested in an Array
+
+// TAKE IT EASY
+
+// Make an array that holds all of the colors of the rainbow.
+const rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
+// Write code that will access "blue" from the rainbow array.
+let blue = rainbow[4];
+console.log(blue);
+
+Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+Write code that will access your hobby from the object that you just created.
